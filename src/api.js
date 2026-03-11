be4f3116bad8dcbc763de1a207a8d1011fc27997
Yashraj <tmkoc1535@gmail.com>
2026-03-11 20:43:23 +0530

    Toast message added

--- src/api.js
+++ src/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
- //baseURL: "https://render-qs89.onrender.com/api/v1.0",
- baseURL: "http://localhost:8080/api/v1.0",
+ baseURL: "https://render-qs89.onrender.com/api/v1.0",
 });
 
 export default api;
